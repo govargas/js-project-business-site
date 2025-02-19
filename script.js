c97c8843cc541eaefe2ff70fbc46fc2b2c1210b0
Talo Vargas <govargas@gmail.com>
2025-02-20 00:44:48 +0100

    js hamburger menu 4 mobile devices via DOM manipulation

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const menuIcon = document.getElementById('menu-icon');
+  const navLinks = document.getElementById('nav-links');
+
+  menuIcon.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    menuIcon.classList.toggle('open');
+  });
+});
